1bcbc9f65a82809fe1f8b3f9ea171692bdf6bedc
schbal50 <schbal50@gmail.com>
2020-09-08 22:49:28 +0200

    REF 5

--- src/Components/MenuItem.js
+++ src/Components/MenuItem.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
 export default function MenuItem(props) {
+
+    function renderIfHasValue(value){
+        if (value) {
+            return <li>value</li>
+        }
+    }
+
     return (
-        <div>
+        <div className="card">
             <li>{props.menuItem.name}</li>
+            {renderIfHasValue(props.menuItem.description)}
+            {renderIfHasValue(props.menuItem.price)}
+            {renderIfHasValue(props.menuItem.categories)}
         </div>
     )
 }
